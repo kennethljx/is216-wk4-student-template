103ddea24206e1bb2f7119a1b167065d21b0cb81
kennethliew <66197803+kennethljx@users.noreply.github.com>
2026-09-07 09:22:23 +0800

    submit ex1 & 2

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,16 +3,18 @@
 
     // Add code here
     // challenge: use array  
+    const message = ref("Choose your favorite fruit: ");
+    const fruits = ref(['apple', 'orange', 'peach']);
     
 </script>
 
 <template>
     {{ message }} 
-   
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+
+    <!-- <label for=""> <input type="radio" name="fruit"> {{ fruits[0] }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[1] }} </label> 
+    <label for=""><input type="radio" name="fruit"> {{ fruits[2] }} </label>  -->
+    <label v-for="fruit in fruits" :key="fruit"><input type="radio" name="fruit"> {{ fruit }}</label>
 </template>
 
 <style scoped>
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,11 +2,15 @@
     import { ref } from 'vue'
 
     // Add code here
+    import img from '/assets/me.png';
+    const width = '200';
+    const height = '200px';
+
 
 </script>
 
 <template>
 
     <!-- Add code here -->
-   
+    <img v-bind:src="img" :width="width" :height="height">
 </template>
