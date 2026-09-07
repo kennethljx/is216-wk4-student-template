f57dfa2c04f48667ca16174f40de37728c3a353c
kennethliew <66197803+kennethljx@users.noreply.github.com>
2026-09-07 10:23:29 +0800

    ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -42,19 +42,19 @@
     <!-- note: need to use single quotes '' for classes such as btn-primary which contains '-' signs
                 because '-' is a minus operator for (Vue) JavaScript  -->
     <div id="part1">
-        <div>
+        <div :id="id" :class="{'blueBox': blueBox, 'redBox': !blueBox}">
             div ID : {{id}} 
         </div>
         
-        <button type="button" v-on:click="changeColor">Change Color</button>
+        <button type="button" v-on:click="changeColor" :class="{'btn-primary': blueBtn, 'btn-danger': !blueBtn}">Change Color</button>
     </div>
 
     <div id="part2">
-        <div>
+        <div :id="id" v-bind:style="{'color':activeColor}">
             div ID : {{id}} 
         </div>
        
-        <button type="button" v-on:click="changeTextColor">Change Text Color</button> 
+        <button type="button" v-on:click="changeTextColor" :class="{'btn-primary': !redBtn, 'btn-danger': redBtn}">Change Text Color</button> 
     </div>
 
 </template>
